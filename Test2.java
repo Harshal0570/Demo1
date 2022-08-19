c0a061c1a91e4ce8a39400149b6e266fe52c19fc
Harshal0570 <111531837+Harshal0570@users.noreply.github.com>
2022-08-19 09:51:20 +0530

    Update and rename Test to Test2.java

---
+++ Test2.java
@@ -0,0 +1,18 @@
+public class Test{
+
+public void fruits(){
+System.out.println("Apple,Banana,Water Melan");
+}
+
+public static void main(String[] args){
+
+Test t1= new Test();
+t1.fruit
+
+
+
+
+}
+
+
+}
